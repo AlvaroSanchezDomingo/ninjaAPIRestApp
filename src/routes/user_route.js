630781b581bad09502c51c1b11e401ae02002fb6
AlvaroSanchezDomingo <alvaro.sanchez.domingo@gmail.com>
2023-05-15 21:57:08 +0200

    add test

--- src/routes/user_route.js
+++ src/routes/user_route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Users = require('../controllers/user_controller');
 
+router.get('/users/test', Users.test);
 router.get('/users/getusers', Users.getusers);
 router.get('/users/getusersById/:userId', Users.getusersById);
 router.post('/users/createUsers', Users.createUsers);
